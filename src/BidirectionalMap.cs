8425f5630c62444cdc9c9b7e9245e3f025fc84ae
Bruno Brant <1890900+bruno-brant@users.noreply.github.com>
2023-04-22 13:39:03 -0300

    [WIP] feat: power spreading

---
+++ src/BidirectionalMap.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (c) Bruno Brant. All rights reserved.
+
+namespace Contagion;
+
+/// <summary>
+/// Allows to associate between pairs of objects and retrieve them in both directions.
+/// </summary>
+/// <typeparam name="T1">
+/// The type of the first object.
+/// </typeparam>
+/// <typeparam name="T2">
+/// The type of the second object.
+/// </typeparam>
+public class BidirectionalMap<T1, T2>
+{
+	// Maps from T1 to T2.
+	private readonly Dictionary<T1, T2> _map1 = new();
+
+	// Maps from T2 to T1.
+	private readonly Dictionary<T2, T1> _map2 = new();
+
+	/// <summary>
+	/// Associate the key1 with the key2.
+	/// </summary>
+	/// <param name="key1">
+	/// The object that will be associated with key2.
+	/// </param>
+	/// <param name="key2">
+	/// The object that will be associated with key1.
+	/// </param>
+	public void Add(T1 key1, T2 key2)
+	{
+		if (_map1.ContainsKey(key1) || _map2.ContainsKey(key2))
+		{
+			throw new ArgumentException("The key is already mapped.");
+		}
+
+		_map1.Add(key1, key2);
+		_map2.Add(key2, key1);
+	}
+
+	/// <summary>
+	/// Get the key mapped from key1.
+	/// </summary>
+	/// <param name="key1">
+	/// The key to map from.
+	/// </param>
+	/// <returns>
+	/// The key mapped from key1.
+	/// </returns>
+	public T2 Get(T1 key1)
+	{
+		if (_map1.TryGetValue(key1, out var key2))
+		{
+			return key2;
+		}
+
+		return default;
+	}
+
+	/// <summary>
+	/// Get the key mapped from key1.
+	/// </summary>
+	/// <param name="key2">
+	/// The key to map from.
+	/// </param>
+	/// <returns>
+	/// The key mapped from key1.
+	/// </returns>
+	public T1 Get(T2 key2)
+	{
+		if (_map2.TryGetValue(key2, out var key1))
+		{
+			return key1;
+		}
+
+		return default;
+	}
+
+	/// <summary>
+	/// Gets the key mapped from key1.
+	/// </summary>
+	/// <param name="key1">
+	/// The key to map from.
+	/// </param>
+	/// <param name="key2">
+	/// The resulting key.
+	/// </param>
+	/// <returns>
+	/// True if the value was found, false otherwise.
+	/// </returns>
+	public bool TryGetValue(T1 key1, out T2 key2)
+	{
+		return _map1.TryGetValue(key1, out key2);
+	}
+
+	/// <summary>
+	/// Gets the key mapped from key2.
+	/// </summary>
+	/// <param name="key2">
+	/// The key to map from.
+	/// </param>
+	/// <param name="key1">
+	/// The resulting key.
+	/// </param>
+	/// <returns>
+	/// True if the value was found, false otherwise.
+	/// </returns>
+	public bool TryGetValue(T2 key2, out T1 key1)
+	{
+		return _map2.TryGetValue(key2, out key1);
+	}
+}
--- src/Scenes/Board/Scripts/SpreadPowerEffect.cs
+++ src/Scenes/Board/Scripts/SpreadPowerEffect.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Bruno Brant. All rights reserved.
+﻿﻿// Copyright (c) Bruno Brant. All rights reserved.
 
 namespace Contagion.Scenes.Board.Scripts;
 
@@ -20,25 +20,25 @@ public class SpreadPowerEffect
 	/// <param name="board">The board to apply the power to.</param>
 	public SpreadPowerEffect(int powerLevel, Board board)
 	{
-		this._powerLevel = powerLevel;
-		this._board = board;
+		_powerLevel = powerLevel;
+		_board = board;
 	}
 
-	///// <summary>
-	///// Applies the power effect to a cell.
-	///// </summary>
-	///// <param name="cell">
-	///// A cell instance to apply the power effect to.
-	///// </param>
-	//public void Apply(Node3D cell) // TODO: Change to Cell.
-	//{
-	//	// Apply power to the cell.
-	//	cell.Set("power_level", _powerLevel);
+	/// <summary>
+	/// Applies the power effect to a cell.
+	/// </summary>
+	/// <param name="cell">
+	/// A cell instance to apply the power effect to.
+	/// </param>
+	public void Apply(Node3D cell) // TODO: Change to Cell.
+	{
+		// Apply power to the cell.
+		cell.Set("power_level", _powerLevel);
 
-	//	// Apply power to adjacent cells.
-	//	foreach (var adjacentCell in this._board.GetAdjacentCells(cell))
-	//	{
-	//		adjacentCell.PowerLevel = this._powerLevel;
-	//	}
-	//}
+		// Apply power to adjacent cells.
+		foreach (var adjacentCell in _board.GetAdjacentCells(cell))
+		{
+			adjacentCell.Set("power_level", _powerLevel - 1);
+		}
+	}
 }
